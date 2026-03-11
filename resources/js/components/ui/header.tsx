d729e951ef6a5f6306060dc2959dbe44bb97fd5a
Greatenishioka <20e1118@andrew.ac.jp>
2026-03-11 14:26:13 +0900

    fix:検索バーの実装開始

--- resources/js/components/ui/header.tsx
+++ resources/js/components/ui/header.tsx
@@ -1,7 +1,79 @@
-export default function Header() {
+import { useForm } from "@inertiajs/react";
+
+type props = {
+    query: string;
+};
+
+export default function Header({ query }: props) {
+
+    const form = useForm({
+        query,
+    });
+
+    function submit(event: React.FormEvent<HTMLFormElement>) {
+        event.preventDefault();
+
+        form.get("/search", {
+            preserveState: true,
+            replace: true,
+        });
+    }
+
     return (
-        <header className="">
-            
+        <header className="bg-[repeating-linear-gradient(45deg,#E3E3E3_0px,#E3E3E3_1px,#FFFFFF_1px,#FFFFFE_4px)] px-20">
+            <div className="rounded-b-full border border-(--color-dark) bg-(--color-light) pt-4 pb-3 h-full flex items-center justify-center">
+                <div className="rounded-full border border-[#9B9B9B] bg-white p-1 max-w-xl w-full">
+                    <label htmlFor="query" className="sr-only">
+
+                    </label>
+                    <form className="flex gap-3" onSubmit={submit}>
+                        <input
+                            id="query"
+                            type="text"
+                            value={form.data.query}
+                            onChange={(event) => form.setData("query", event.target.value)}
+                            className="w-full rounded px-4 py-2"
+                            placeholder="artificial intelligence"
+                        />
+                        <div className="flex">
+                            <button
+                                type="submit"
+                                disabled={form.processing}
+                                className="rounded-r-full bg-[#2BB2C6] pr-4 py-2 text-white disabled:opacity-60 relative"
+                            >
+                                <div className="overflow-hidden h-full aspect-square absolute -translate-x-full top-0">
+                                    <span className=" inset-0 h-[200%] aspect-square rounded-full bg-[#2BB2C6] block"></span>
+                                </div>
+                                <div className="flex items-center">
+                                    <svg className="mr-1" width="12" height="12" viewBox="0 0 12 12" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                        <path d="M11.0667 12L6.86667 7.8C6.53333 8.06667 6.15 8.27778 5.71667 8.43333C5.28333 8.58889 4.82222 8.66667 4.33333 8.66667C3.12222 8.66667 2.09722 8.24722 1.25833 7.40833C0.419445 6.56944 0 5.54444 0 4.33333C0 3.12222 0.419445 2.09722 1.25833 1.25833C2.09722 0.419445 3.12222 0 4.33333 0C5.54444 0 6.56944 0.419445 7.40833 1.25833C8.24722 2.09722 8.66667 3.12222 8.66667 4.33333C8.66667 4.82222 8.58889 5.28333 8.43333 5.71667C8.27778 6.15 8.06667 6.53333 7.8 6.86667L12 11.0667L11.0667 12ZM4.33333 7.33333C5.16667 7.33333 5.875 7.04167 6.45833 6.45833C7.04167 5.875 7.33333 5.16667 7.33333 4.33333C7.33333 3.5 7.04167 2.79167 6.45833 2.20833C5.875 1.625 5.16667 1.33333 4.33333 1.33333C3.5 1.33333 2.79167 1.625 2.20833 2.20833C1.625 2.79167 1.33333 3.5 1.33333 4.33333C1.33333 5.16667 1.625 5.875 2.20833 6.45833C2.79167 7.04167 3.5 7.33333 4.33333 7.33333Z" fill="white" />
+                                    </svg>
+                                    <p className="text-[14px] mr-2">
+                                        Search
+                                    </p>
+                                    <span className="whitespace-nowrap text-[10px]">
+                                        検索
+                                    </span>
+                                </div>
+                            </button>
+                            <button
+                                type="submit"
+                                disabled={form.processing}
+                                className="rounded-r-full bg-zinc-900 pr-4 py-2 text-white disabled:opacity-60 relative"
+                            >
+                                <div className="flex items-center">
+                                    <p className="text-[14px] mr-2 whitespace-nowrap">
+                                        Fuzzy search
+                                    </p>
+                                    <span className="whitespace-nowrap text-[10px]">
+                                        あいまい検索
+                                    </span>
+                                </div>
+                            </button>
+                        </div>
+                    </form>
+                </div>
+            </div>
         </header>
     );
 }
