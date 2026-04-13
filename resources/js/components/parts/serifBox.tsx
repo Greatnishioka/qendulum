bcee9c6e2db4396313b7e00a9d26d35189beb49f
Greatenishioka <20e1118@andrew.ac.jp>
2026-04-13 23:44:10 +0900

    吹き出しボックスの出現時アニメーションを追加(ただしまだ動かない)

--- resources/js/components/parts/serifBox.tsx
+++ resources/js/components/parts/serifBox.tsx
@@ -18,14 +18,36 @@ type props = {
 
 export default function SerifBox({ children, setIsOpenModal, isOpen, title, position, drawingArea }: props) {
     return (
-        <div
-            className={`fixed z-50 flex -translate-x-full -translate-y-1/2 justify-center items-center transition-all duration-250 ${isOpen ? "opacity-100" : "pointer-events-none opacity-0 -translate-y-[calc(50%+8px)]"}`}
+        <motion.div
+            className={`fixed z-50 flex -translate-x-full -translate-y-1/2 justify-center items-center ${isOpen ? "" : "pointer-events-none"}`}
             style={{ top: position.top, left: position.left }}
+            initial={false}
+            animate={{
+                opacity: isOpen ? 1 : 0,
+                y: isOpen ? 0 : 10,
+            }}
+            transition={{
+                opacity: { duration: 0.16, ease: "easeOut" },
+                y: { duration: 0.3, ease: [0.22, 1, 0.36, 1] },
+            }}
         >
-            <div
+            <motion.div
                 className="
                 relative overflow-hidden rounded-2xl border border-(--color-dark) qendulum-shadow
             "
+                style={{ transformOrigin: "100% 50%" }}
+                initial={false}
+                animate={{
+                    scaleX: isOpen ? 1 : 0.08,
+                    scaleY: isOpen ? 1 : 0.18,
+                    x: isOpen ? 0 : 22,
+                    skewY: isOpen ? 0 : -7,
+                    filter: isOpen ? "blur(0px)" : "blur(1.5px)",
+                }}
+                transition={{
+                    duration: isOpen ? 0.42 : 0.28,
+                    ease: isOpen ? [0.16, 1, 0.3, 1] : [0.4, 0, 1, 1],
+                }}
             >
                 <div className="relative z-20 py-1.5 px-5 flex justify-between border-b border-(--color-dark) bg-white">
                     <div className="relative min-w-20 h-4.5">
@@ -60,19 +82,37 @@ export default function SerifBox({ children, setIsOpenModal, isOpen, title, posi
                 </div>
                 <div className="relative z-10 p-1 bg-white">
                     <div className="rounded-b-2xl rounded-t-sm py-1.5 bg-[#EDEDED] flex items-start justify-center px-8">
-                        <div style={{ width: drawingArea?.width, height: drawingArea?.height }} className="overflow-hidden">
+                        <motion.div
+                            style={{ width: drawingArea?.width, height: drawingArea?.height }}
+                            className="overflow-hidden"
+                            initial={false}
+                            animate={{ opacity: isOpen ? 1 : 0.45 }}
+                            transition={{ duration: isOpen ? 0.2 : 0.12, ease: "easeOut" }}
+                        >
                             {children}
-                        </div>
+                        </motion.div>
                     </div>
                 </div>
-            </div>
-            <svg
+            </motion.div>
+            <motion.svg
                 className="relative z-20 -left-0.5"
                 width="29"
                 height="14"
                 viewBox="0 0 29 14"
                 fill="none"
                 xmlns="http://www.w3.org/2000/svg"
+                style={{ transformOrigin: "100% 50%" }}
+                initial={false}
+                animate={{
+                    scaleX: isOpen ? 1 : 0.14,
+                    scaleY: isOpen ? 1 : 0.4,
+                    x: isOpen ? 0 : 10,
+                    opacity: isOpen ? 1 : 0,
+                }}
+                transition={{
+                    duration: isOpen ? 0.34 : 0.22,
+                    ease: isOpen ? [0.16, 1, 0.3, 1] : [0.4, 0, 1, 1],
+                }}
             >
                 <path
                     d="M0.000148773 -1.26082e-06L27.1755 4.56428C29.4005 4.93797 29.4005 8.13533 27.1755 8.50902L0.000148202 13.0733L0.000148773 -1.26082e-06Z"
@@ -82,7 +122,7 @@ export default function SerifBox({ children, setIsOpenModal, isOpen, title, posi
                     d="M0.918945 0.154052L27.1757 4.56414C29.4004 4.93795 29.4004 8.13564 27.1757 8.50945L0.918945 12.9195L0.918945 11.9055L27.0096 7.52312C28.1221 7.33628 28.1221 5.73732 27.0096 5.55047L0.918945 1.16811L0.918945 0.154052Z"
                     fill="#E3E3E3"
                 />
-            </svg>
-        </div>
+            </motion.svg>
+        </motion.div>
     );
 }
